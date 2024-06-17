f2ab5b302a5b9b0a5551eaf59accc934e360ee8c
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2024-06-16 19:17:07 -0500

    v0.0.3: abstract cacheFetch;

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,3 +1,18 @@
 export function uuid() {
   return Math.random().toString(36).substring(2) + Date.now().toString(36);
 }
+
+export async function cacheFetch(url, store) {
+  const previousResponse = store.findMatchingItemByKey({key: 'url', value: url})?.response;
+  if (previousResponse) {
+    console.log('returning from cache', url);
+    return previousResponse;
+  }
+
+  console.log('fetching', url);
+  const response = await fetch(url);
+  const data = await response.json();
+  store.add({url, response: data});
+
+  return data;
+}
